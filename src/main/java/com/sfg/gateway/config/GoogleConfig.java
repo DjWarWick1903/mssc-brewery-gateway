b6f02433f3dc3ef97cfbbc883cc094d602bc688d
Popescu Robert Ionut <robertpopescu1903@gmail.com>
2022-09-15 00:21:34 +0300

    Create example gateway

---
+++ src/main/java/com/sfg/gateway/config/GoogleConfig.java
@@ -0,0 +1,25 @@
+package com.sfg.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * Configuration class made as example to demonstrate Spring Cloud Gateway
+ */
+@Profile("google")
+@Configuration
+public class GoogleConfig {
+
+    @Bean
+    public RouteLocator googleGateway(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/googlesearch2")
+                        .filters(f -> f.rewritePath("/googlesearch2(?<segment>/?.*)", "/${segment}"))
+                        .uri("https://google.com")
+                        .id("google"))
+                .build();
+    }
+}
